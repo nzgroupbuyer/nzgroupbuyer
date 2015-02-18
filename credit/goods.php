414d1171edd1e280b7ee26522b6f781b133e3f46
prl <fairypearl.d@hotmail.com>
2015-02-18 17:34:19 +1300

    init2
    init2 18/02/15

---
+++ credit/goods.php
@@ -0,0 +1,17 @@
+<?php
+require_once(dirname(dirname(__FILE__)) . '/app.php');
+
+need_login();
+$condition = array();
+$count = Table::Count('goods', $condition);
+list($pagesize, $offset, $pagestring) = pagestring($count, 20);
+
+$goods = DB::LimitQuery('goods', array(
+			'condition'=>$condition,
+			'size' => $pagesize,
+			'offset' => $offset,
+			'order' => 'ORDER BY id DESC',
+			));
+
+$pagetitle = '兑换商品';
+include template('credit_goods');
